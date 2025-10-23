040d5423bb45c9c4d9ebd28e75ca48ca7d3e99b1
Lasse Keijonen <lasse.keijonen@aalto.fi>
2025-10-23 16:18:25 +0300

    SFML display setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;  
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML App");
+    
+    sf::RectangleShape shape;
+
+    shape.setSize(sf::Vector2f(50,50));
+    shape.setPosition(250,250);
+
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    
     return 0;
 }
