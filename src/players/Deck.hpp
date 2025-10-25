754665c0c0fc75d2ee50c274be7e07a27b1705a4
Oskari Niini <oskari.niini@aalto.fi>
2025-10-25 13:29:19 +0300

    first version of Deck class header

---
+++ src/players/Deck.hpp
@@ -0,0 +1,18 @@
+#ifndef DECK_HPP
+#define DECK_HPP
+
+#include <vector>
+#include "Card.hpp"
+
+class Deck {
+public:
+  Deck();
+  void addCard(const Card& card);
+  void removeCard(const Card& card);
+  const std::vector<Card>& getCards() const;
+
+private:
+  std::vector<Card> cards;
+};
+
+#endif
