eb1149236df296d477ba8f86d83e8a0f139f8a54
Oskari Niini <oskari.niini@aalto.fi>
2025-10-25 15:16:12 +0300

    first version of Building class header

---
+++ src/entities/Building.hpp
@@ -0,0 +1,25 @@
+#ifndef BUILDING_HPP
+#define BUILDING_HPP
+
+#include "Entity.hpp"
+
+class Building : public Entity {
+protected:
+  int health_;
+  int damage_;
+  float attackCooldown_;
+  float attackRange_;
+  int width_;
+  int height_;
+
+public:
+  Building(int x, int y) : Entity(x, y) {}
+
+  virtual void move() = 0;
+  virtual void attack() = 0;
+
+  void takeDamage(int amount);
+
+  void die();
+};
+#endif
