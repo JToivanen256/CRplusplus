bfbf1c168f543f5406c98c88c7d7abf6580c6cf6
Oskari Niini <oskari.niini@aalto.fi>
2025-10-25 13:29:35 +0300

    first version of Hand class header

---
+++ src/players/Hand.hpp
@@ -0,0 +1,18 @@
+#ifndef HAND_HPP
+#define HAND_HPP
+
+#include <vector>
+#include "Card.hpp"
+
+class Hand {
+public:
+  Hand();
+  void addCard(const Card& card);
+  void removeCard(const Card& card);
+  const std::vector<Card>& getCards() const;
+
+private:
+  std::vector<Card> cards;
+};
+
+#endif
