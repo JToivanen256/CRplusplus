92dc2ef4a3151b7766ed1ef38b25d07092270e2d
Oskari Niini <oskari.niini@aalto.fi>
2025-10-25 16:20:20 +0300

    first version of Match class header

---
+++ src/game/Match.hpp
@@ -0,0 +1,19 @@
+#ifndef MATCH_HPP
+#define MATCH_HPP
+
+#include "../players/Player.hpp"
+#include "Map.hpp"
+
+
+class Match {
+private:
+  Player player1_;
+  Player player2_;
+  Map map_;
+
+public:
+  Match(Player player1, Player player2, Map map);
+
+};
+
+#endif
